9d36aa1d990e7ed409aacb480d9f52c4c952bf5b
Merijn <>
2026-07-21 14:23:37 +0200

    Remove unused code from recipes

--- recipes/forms.py
+++ recipes/forms.py
@@ -37,22 +37,3 @@ class RecipeIngredientForm(forms.ModelForm):
         if user:
 
             self.fields["food"].queryset = get_user_foods(user)
-
-
-class AddRecipeToDiaryForm(forms.Form):
-
-    meal = forms.ModelChoiceField(queryset=None)
-
-    portions = forms.FloatField(initial=1, min_value=0.1)
-
-    def __init__(self, *args, user=None, **kwargs):
-
-        super().__init__(*args, **kwargs)
-
-        from diary.models import Meal
-
-        if user:
-
-            self.fields["meal"].queryset = Meal.objects.filter(user=user).order_by(
-                "-date", "order"
-            )
