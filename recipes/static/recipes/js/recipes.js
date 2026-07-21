fc03b7a8c95e8d4ddcb4efc13c20476fd8224121
Merijn <>
2026-07-21 16:53:42 +0200

    Change how modal is reloaded

--- recipes/static/recipes/js/recipes.js
+++ recipes/static/recipes/js/recipes.js
@@ -41,45 +41,45 @@ function toggleFilter(name) {
     input.value = input.value === "1" ? "0" : "1";
 }
 
-document.addEventListener("htmx:configRequest", function (event) {
-    const modal = document.getElementById("tagsModal");
+// document.addEventListener("htmx:configRequest", function (event) {
+//     const modal = document.getElementById("tagsModal");
 
-    if (modal && modal.classList.contains("show")) {
-        event.detail.headers["X-Tags-Modal-Open"] = "true";
-    }
-});
+//     if (modal && modal.classList.contains("show")) {
+//         event.detail.headers["X-Tags-Modal-Open"] = "true";
+//     }
+// });
 
 
-document.body.addEventListener("htmx:afterSettle", function () {
-    const modalElement = document.getElementById("tagsModal");
+// document.body.addEventListener("htmx:afterSettle", function () {
+//     const modalElement = document.getElementById("tagsModal");
 
-    if (!modalElement) {
-        return;
-    }
+//     if (!modalElement) {
+//         return;
+//     }
 
-    if (modalElement.dataset.keepOpen !== "true") {
-        return;
-    }
+//     if (modalElement.dataset.keepOpen !== "true") {
+//         return;
+//     }
 
-    const oldModal = bootstrap.Modal.getInstance(modalElement);
+//     const oldModal = bootstrap.Modal.getInstance(modalElement);
 
-    if (oldModal) {
-        oldModal.dispose();
-    }
+//     if (oldModal) {
+//         oldModal.dispose();
+//     }
 
-    document.querySelectorAll(".modal-backdrop").forEach((backdrop) => {
-        backdrop.remove();
-    });
+//     document.querySelectorAll(".modal-backdrop").forEach((backdrop) => {
+//         backdrop.remove();
+//     });
 
-    document.body.classList.remove("modal-open");
-    document.body.style.removeProperty("padding-right");
+//     document.body.classList.remove("modal-open");
+//     document.body.style.removeProperty("padding-right");
 
-    const modal = new bootstrap.Modal(modalElement, {
-        backdrop: true,
-        focus: true,
-    });
+//     const modal = new bootstrap.Modal(modalElement, {
+//         backdrop: true,
+//         focus: true,
+//     });
 
-    modal.show();
+//     modal.show();
 
-    delete modalElement.dataset.keepOpen;
-});
+//     delete modalElement.dataset.keepOpen;
+// });
