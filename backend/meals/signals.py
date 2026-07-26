a5ad32657c8035b86b2cb785ee7ab7c3a737b985
Merijn <>
2026-07-26 11:16:02 +0200

    Remove imports that didn't exist

--- backend/meals/signals.py
+++ backend/meals/signals.py
@@ -4,10 +4,6 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .defaults import DEFAULT_MEALS
-
-from django.contrib.auth.models import User
-
 from .models import DefaultMeal
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
